4a335fca2200122ed9d32a29cf001b3251271e2d
Sahil Goel <sahilgoel2013@yahoo.com>
2023-02-12 17:44:04 -0500

    Created test case for map getloc method

--- Projects/P2/app/src/test/java/pacman/TestMapGetLoc.java
+++ Projects/P2/app/src/test/java/pacman/TestMapGetLoc.java
@@ -1,11 +1,21 @@
 package pacman;
 import java.io.*;
+
+import javax.xml.stream.Location;
+
 import junit.framework.*;
 import java.awt.Color;
 
 public class TestMapGetLoc extends TestCase {
 
   public void testMapGetLoc() throws FileNotFoundException {
+    Mainframe frame = new MainFrame();
+    PacMan pacman = frame.addPacMan(new Location(1, 2));
+
+    if (!((frame.myMap.getLoc(new Location(1, 2))).contains(Map.Type.PACMAN))) {
+      throw new FileNotFoundException();
+    }
+    
     return;
   }
 }
