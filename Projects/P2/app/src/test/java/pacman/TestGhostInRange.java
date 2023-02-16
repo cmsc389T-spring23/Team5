d13b1fc52d88987ad43b1bcdbe19c917c13f6ba3
Andrew Liu <aliu12076@gmail.com>
2023-02-16 12:24:14 -0500

    added test for ghost in range

--- Projects/P2/app/src/test/java/pacman/TestGhostInRange.java
+++ Projects/P2/app/src/test/java/pacman/TestGhostInRange.java
@@ -6,6 +6,17 @@ import java.awt.Color;
 public class TestGhostInRange extends TestCase {
 
   public void testGhostInRange() throws FileNotFoundException {
+    Mainframe frame = new MainFrame();
+    PacMan pacman = frame.addPacMan(new Location(2, 2)); 
+
+    if (pacman.is_ghost_in_range()) {
+      throw new FileNotFoundException();
+    }
+    Ghost leftGhost = frame.addGhost(new Location(1,2), "Andrew", Color.red);
+    if (!pacman.is_ghost_in_range()) {
+      throw new FileNotFoundException();
+    }
+
     return;
   }
 }
