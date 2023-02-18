f80217abfd9d27cfee99774902d545eee7c70ccf
malchu <malchupascual@gmail.com>
2023-02-18 01:13:06 -0500

    ghost-attack

--- Projects/P2/app/src/test/java/pacman/TestGhostAttack.java
+++ Projects/P2/app/src/test/java/pacman/TestGhostAttack.java
@@ -6,6 +6,11 @@ import java.awt.Color;
 public class TestGhostAttack extends TestCase {
 
   public void testGhostAttack() throws FileNotFoundException {
-    return;
+    Mainframe frame = new Mainframe();
+    PacMan pacman = frame.addPacMan(new Location(0,0));
+    Ghost ghost = frame.addGhost(new Location(0,0), "clyde", ghost.colors[1]);
+    if (ghost.attack() == false) {
+      throw new FileNotFoundException();
+    }
   }
 }
