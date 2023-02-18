9ee22ad4e8415e9907101c0678824553da39733e
malchu <malchupascual@gmail.com>
2023-02-18 01:34:13 -0500

    map eatcookie test

--- Projects/P2/app/src/test/java/pacman/TestMapEatCookie.java
+++ Projects/P2/app/src/test/java/pacman/TestMapEatCookie.java
@@ -6,6 +6,11 @@ import java.awt.Color;
 public class TestMapEatCookie extends TestCase {
 
   public void testMapEatCookie() throws FileNotFoundException {
-    return;
+    Mainframe frame = new Mainframe();
+    PacMan pacman = frame.addPacMan(new Location(0,0));
+    Map.add("tok_x0_y0", new Location(0,0), new CookieComponent(0,0,1), Map.Type.COOKIE);
+    if (Map.eatCookie("tok_x0_y0") == false) {
+      throw new FileNotFoundException();
+    }
   }
 }
